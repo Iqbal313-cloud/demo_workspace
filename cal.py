ac560ec12facd5a9a3b9bf73896f7be9148c5194
Iqbal <muhammadiqbalxyz251@gmail.com>
2023-06-15 17:59:35 +0600

    committing py files

---
+++ cal.py
@@ -0,0 +1,10 @@
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operartor")
